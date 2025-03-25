1a35492805a691a3de56fe39dbd2b5fec4e2fabc
ayman <danishaiman3b@gmail.com>
2025-03-25 14:38:29 +0800

    add XML documentation to Site.Master.cs for improved clarity and maintainability

--- Site.Master.cs
+++ Site.Master.cs
@@ -2,14 +2,14 @@ using System;
 using System.Web.UI;
 using System.Web.Security;
 
-
-//THIS IS THE NAVBAR
 namespace FETS
 {
+    /// <summary>
+    /// Master page that implements the site's main navigation and layout
+    /// Contains the navbar and sidebar navigation elements
+    /// </summary>
     public partial class SiteMaster : MasterPage
     {
-
-
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -18,29 +18,33 @@ namespace FETS
                 {
                     lblUsername.Text = Context.User.Identity.Name;
                     
-                    // Set active page in sidebar based on current URL
+                    // Highlight the current page in the navigation sidebar
                     SetActivePage();
                 }
                 else
                 {
+                    // Redirect unauthenticated users to login page
                     Response.Redirect("~/Default.aspx");
                 }
             }
         }
         
-        // Add this new method to set the active page
+        /// <summary>
+        /// Sets the active navigation link based on the current URL
+        /// Adds the "active" CSS class to the appropriate navigation button
+        /// </summary>
         private void SetActivePage()
         {
             string currentUrl = Request.Url.AbsolutePath.ToLower();
             
-            // Reset all navigation links
+            // Reset all navigation links to default state
             btnDashboard.CssClass = "nav-link";
             btnDataEntry.CssClass = "nav-link";
             btnViewSection.CssClass = "nav-link";
             btnMapLayout.CssClass = "nav-link";
             btnProfile.CssClass = "nav-link";
             
-            // Set active class based on current URL
+            // Set active class for the current page
             if (currentUrl.Contains("/dashboard/"))
             {
                 btnDashboard.CssClass = "nav-link active";
@@ -63,6 +67,9 @@ namespace FETS
             }
         }
 
+        /// <summary>
+        /// Navigation event handlers for directing users to different system pages
+        /// </summary>
         protected void btnDashboard_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/Pages/Dashboard/Dashboard.aspx");
@@ -88,6 +95,9 @@ namespace FETS
             Response.Redirect("~/Pages/Profile/Profile.aspx");
         }
 
+        /// <summary>
+        /// Signs out the current user and redirects to login page
+        /// </summary>
         protected void btnLogout_Click(object sender, EventArgs e)
         {
             FormsAuthentication.SignOut();
