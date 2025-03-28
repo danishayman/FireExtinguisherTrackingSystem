c08f7a2d5c212521ee8870969e2d7a7c84b71d6b
ayman <danishaiman3b@gmail.com>
2025-03-28 11:03:29 +0800

    Add Plants and Users tables with foreign key relationship; drop Users table if it exists

--- DATABASE/DB_Updates.sql
+++ DATABASE/DB_Updates.sql
@@ -16,3 +16,40 @@ CREATE TABLE ServiceReminders (
 -- Create index for efficient querying
 CREATE INDEX IX_ServiceReminders_ReminderDate 
 ON ServiceReminders(ReminderDate, ReminderSent); 
+
+
+
+
+
+
+
+
+
+
+
+
+-- First drop the Users table if it exists
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]'))
+    DROP TABLE [dbo].[Users];
+GO
+
+-- Create Plants table first
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Plants]'))
+BEGIN
+    CREATE TABLE [dbo].[Plants] (
+        [PlantID] INT IDENTITY(1,1) PRIMARY KEY,
+        [PlantName] NVARCHAR(100) NOT NULL
+    );
+END
+GO
+
+-- Then create Users table with foreign key reference
+CREATE TABLE [dbo].[Users] (
+    [UserID] INT IDENTITY(1,1) PRIMARY KEY,
+    [Username] NVARCHAR(50) NOT NULL UNIQUE,
+    [PasswordHash] VARBINARY(256) NOT NULL,
+    [Role] NVARCHAR(20) NOT NULL,
+    [PlantID] INT NULL,
+    FOREIGN KEY ([PlantID]) REFERENCES [Plants]([PlantID])
+);
+GO
